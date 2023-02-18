e032cb7b9f5e358c6d505bb709d5cfc66c0fa73b
AlanXZhang <72415499+AlanXZhang@users.noreply.github.com>
2023-02-17 16:48:21 -0800

    Added text preprocessing

---
+++ src/text_preprocessing.py
@@ -0,0 +1,94 @@
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+lemmatizer = WordNetLemmatizer()
+
+
+# lemmatize string
+def lemmatize_word(text):
+    word_tokens = word_tokenize(text)
+    # provide context i.e. part-of-speech
+    lemmas = [lemmatizer.lemmatize(word, pos ='v') for word in word_tokens]
+    return lemmas
+
+def text_lowercase(text):
+    return text.lower()
+
+def clean_numbers(x):
+    return re.sub(r'\d+', '', x)
+
+def remove_punctuation(text):
+    translator = str.maketrans('', '', string.punctuation)
+    return text.translate(translator)
+
+def remove_stopwords(text):
+    stop_words = set(stopwords.words("english"))
+    word_tokens = word_tokenize(text)
+    filtered_text = [word for word in word_tokens if word not in stop_words]
+    return filtered_text
+
+def remove_whitespace(text):
+    return  " ".join(text.split())
+
+def remove_contractions(text):
+    """Remove contractions from a text string"""
+    contractions = {
+        "ain't": "is not",
+        "aren't": "are not",
+        "can't": "cannot",
+        "could've": "could have",
+        "couldn't": "could not",
+        "didn't": "did not",
+        "doesn't": "does not",
+        "don't": "do not",
+        "hadn't": "had not",
+        "hasn't": "has not",
+        "haven't": "have not",
+        "he'd": "he would",
+        "he'll": "he will",
+        "he's": "he is",
+        "I'd": "I would",
+        "I'll": "I will",
+        "I'm": "I am",
+        "I've": "I have",
+        "isn't": "is not",
+        "it's": "it is",
+        "let's": "let us",
+        "might've": "might have",
+        "must've": "must have",
+        "mustn't": "must not",
+        "shan't": "shall not",
+        "she'd": "she would",
+        "she'll": "she will",
+        "she's": "she is",
+        "should've": "should have",
+        "shouldn't": "should not",
+        "that's": "that is",
+        "there's": "there is",
+        "they'd": "they would",
+        "they'll": "they will",
+        "they're": "they are",
+        "they've": "they have",
+        "wasn't": "was not",
+        "we'd": "we would",
+        "we'll": "we will",
+        "we're": "we are",
+        "we've": "we have",
+        "weren't": "were not",
+        "what'll": "what will",
+        "what're": "what are",
+        "what's": "what is",
+        "what've": "what have",
+        "where's": "where is",
+        "who'll": "who will",
+        "who's": "who is",
+        "who've": "who have",
+        "won't": "will not",
+        "would've": "would have",
+        "wouldn't": "would not",
+        "you'd": "you would",
+        "you'll": "you will",
+        "you're": "you are",
+        "you've": "you have"
+    }
+    clean_text = ' '.join([contractions.get(word, word) for word in text.split()])
+    return clean_text
